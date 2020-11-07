94dfedac8555d8128f18e9d7770bbdb813d568b6
Dominique Pendjol <d.pendjol@gmail.com>
2020-11-07 18:34:20 +0100

    Cleaned up the clean install

---
+++ src/components/InputForm/index.js
@@ -0,0 +1,16 @@
+import React from './react';
+import {Component} from './react'
+
+class InputForm extends Component {
+    constructor() {
+        super()
+    }
+
+    renders() {
+        return (
+            <h1> Dit is een test.</h1>
+        )
+    }
+}
+
+export default InputForm
