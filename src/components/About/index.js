883d64564e08adc44017a71fc9d280c85f30c9c2
Dominique Pendjol <d.pendjol@gmail.com>
2020-11-10 16:31:08 +0100

    react-router-dom geimplementeerd in de pagina

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,18 @@
+import './About.css'
+
 const About = () => {
     return (
-    <h1> About This </h1>
+    <div className="about__container">
+        <h1 className="about__title">
+            Over dit project 
+        </h1>
+        <h2 className="about__subtitle"> 
+            Gebruik van React en React Router
+        </h2>
+        <p className="about__paragraph">
+            Dit project is om te oefenen met het opzetten van een project met React om bekend te raken met de mogelijkheden hiervan.
+        </p>
+    </div>
     )
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
+import './Nav.css'
 import {NavLink} from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <div>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/about'>About</NavLink>
-        </div>
+        <nav className="navigation">
+            <h3>Navigatie:</h3>
+            <ul className="navigation__list">
+                <NavLink exact to='/' activeClassName="navigation__item--active">
+                    <li className="navigation__item">Home</li>
+                </NavLink>
+                <NavLink to='/about' activeClassName="navigation__item--active">
+                    <li className="navigation__item">
+                        About
+                    </li>
+                </NavLink>
+            </ul>
+        </nav>
     )
 }
 
