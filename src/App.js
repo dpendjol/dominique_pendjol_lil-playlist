512edd2fda5c80e80b6d756d08f0d18a561dcd19
Dominique Pendjol <d.pendjol@gmail.com>
2020-11-10 15:21:51 +0100

    Added react-routing-dom to react and implemented it very simple

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import './App.css';
 import Container from './components/Container'
 import Header from './components/Header'
 
+import Nav from './components/Nav'
+import About from './components/About'
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 function App() {
 	return (
-    	<div className="App">
-			<Header />
-			<Container />
-    	</div>
+		<Router>
+			<div className="App">
+				<Header />
+				<Nav />
+				<Switch>
+					<Route exact path="/" component={Container} />
+					<Route exact path="/about" component={About} />
+				</Switch>
+			</div>
+		</Router>
   	);
 }
 
